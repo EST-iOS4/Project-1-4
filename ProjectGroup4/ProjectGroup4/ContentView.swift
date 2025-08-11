07a65f0b3601fc4ee79dd8460455b90b87fd8a53
황동혁 <ghkdehdgur01@gmail.com>
2025-08-11 10:24:14 +0900

    xcode파일 추가

---
+++ ProjectGroup4/ProjectGroup4/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ProjectGroup4
+//
+//  Created by 황동혁 on 8/11/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ProjectGroup4/ProjectGroup4/ProjectGroup4App.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectGroup4App.swift
+//  ProjectGroup4
+//
+//  Created by 황동혁 on 8/11/25.
+//
+
+import SwiftUI
+
+@main
+struct ProjectGroup4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
