841bf563c912b4ef8928d5dc91b1811567a04389
황동혁 <ghkdehdgur01@gmail.com>
2025-08-12 12:39:27 +0900

    파일 구조 업데이트

---
+++ ProjectGroup4/ProjectGroup4/Model/QuizResult.swift
@@ -0,0 +1,13 @@
+//
+//  Quiz.swift
+//  ProjectGroup4
+//
+//  Created by 황동혁 on 8/12/25.
+//
+
+struct QuizResult{
+    let image: String
+    let answer : String
+    let correctAnswer : String
+    let isCorrect : Bool
+}
