117cb6c47603e8303103875fb79ec8fb76e6d93b
kmw4561@gmail.com <kmw4561@gmail.com>
2025-08-13 17:14:35 +0900

    update QuizData

--- ProjectGroup4/ProjectGroup4/Data/QuizData.swift
+++ ProjectGroup4/ProjectGroup4/Data/QuizData.swift
@@ -9,125 +9,128 @@ import Foundation
 
 struct QuizData {
     // 동물 카테고리
-    static let animals_easy: [String: String] = [
-        "개": "개",
-        "고양이": "고양이",
-        "거북": "거북",
-        "기린": "기린",
-        "돼지": "돼지",
-        "사자": "사자",
-        "소": "소",
-        "여우": "여우",
-        "원숭이": "원숭이",
-        "호랑이": "호랑이"
+    static let animals_easy: [ImageName: UserAnswer] = [
+        "Dog1": "개",
+        "Cat1": "고양이",
+        "Turtle1": "거북",
+        "Giraffe1": "기린",
+        "Pig1": "돼지",
+        "Lion1": "사자",
+        "Cow1": "소",
+        "Fox1": "여우",
+        "Monkey1": "원숭이",
+        "Tiger1": "호랑이"
     ]
     
-    static let animals_medium: [String: String] = [
-        "나무늘보": "나무늘보",
-        "당나귀": "당나귀",
-        "라마": "라마",
-        "물소": "물소",
-        "알파카": "알파카",
-        "염소": "염소",
-        "울버린": "울버린",
-        "카피바라": "카피바라",
-        "쿼카": "쿼카",
-        "흑염소": "흑염소"
+    static let animals_medium: [ImageName: UserAnswer] = [
+        "Tree2": "나무늘보",
+        "Donkey2": "당나귀",
+        "Lama2": "라마",
+        "Rhino2": "물소",
+        "Alpaca2": "알파카",
+        "Goat2": "염소",
+        "Wolverine2": "울버린",
+        "Capybara2": "카피바라",
+        "Quokka2": "쿼카",
+        "BlackGoat2": "흑염소"
     ]
     
-    static let animals_hard: [String: String] = [
-        "두공": "두공",
-        "라쿤독": "라쿤독",
-        "사올라": "사올라",
-        "아르마딜로": "아르마딜로",
-        "아이아이": "아이아이",
-        "오셀롯": "오셀롯",
-        "오카피": "오카피",
-        "카카포": "카카포",
-        "테이퍼": "테이퍼",
-        "팡골린": "팡골린"
+    static let animals_hard: [ImageName: UserAnswer] = [
+        "Dugong3": "두공",
+        "RaccoonDog3": "라쿤독",
+        "Saola3": "사올라",
+        "Armadillo3": "아르마딜로",
+        "AyeAye3": "아이아이",
+        "Ocelot3": "오셀롯",
+        "Okapi3": "오카피",
+        "Kakapo3": "카카포",
+        "Tapir3": "테이퍼",
+        "Pangolin3": "팡골린"
     ]
     
     // 연예인 카테고리
-    static let celebrities_easy: [String: String] = [
-        "뷔": "뷔",
-        "손예진": "손예진",
-        "송중기": "송중기",
-        "아이유": "아이유",
-        "윤아": "윤아",
-        "전지현": "전지현",
-        "제니": "제니",
-        "지드래곤": "지드래곤",
-        "차은우": "차은우",
-        "태연": "태연"
+    static let celebrities_easy: [ImageName: UserAnswer] = [
+        "V1": "뷔",
+        "SonYejin1": "손예진",
+        "SongJoongki1": "송중기",
+        "IU1": "아이유",
+        "Yuna1": "윤아",
+        "JunJihyun1": "전지현",
+        "Jennie1": "제니",
+        "GDragon1": "지드래곤",
+        "ChaEunwoo1": "차은우",
+        "Taeyeon1": "태연"
     ]
     
-    static let celebrities_medium: [String: String] = [
-        "류준열": "류준열",
-        "민지": "민지",
-        "박보검": "박보검",
-        "박형식": "박형식",
-        "안유진": "안유진",
-        "윈터": "윈터",
-        "주지훈": "주지훈",
-        "카리나": "카리나",
-        "박신혜": "박신혜",
-        "혜리": "혜리"
+    static let celebrities_medium: [ImageName: UserAnswer] = [
+        "RyuJunyeol2": "류준열",
+        "Minji2": "민지",
+        "ParkBogum2": "박보검",
+        "ParkHyungsik2": "박형식",
+        "AnYujin2": "안유진",
+        "Winter2": "윈터",
+        "JuJihun2": "주지훈",
+        "Karina2": "카리나",
+        "ParkShinhye2": "박신혜",
+        "Hyeri2": "혜리"
     ]
     
-    static let celebrities_hard: [String: String] = [
-        "공명": "공명",
-        "김재중": "김재중",
-        "닝닝": "닝닝",
-        "리즈": "리즈",
-        "붐": "붐",
-        "안보현": "안보현",
-        "옥택연": "옥택연",
-        "유준상": "유준상",
-        "정용화": "정용화",
-        "한예슬": "한예슬"
+    static let celebrities_hard: [ImageName: UserAnswer] = [
+        "GongMyung3": "공명",
+        "KimJaejoong3": "김재중",
+        "Ningning3": "닝닝",
+        "Liz3": "리즈",
+        "Boom3": "붐",
+        "AhnBohyun3": "안보현",
+        "OkTaecyeon3": "옥택연",
+        "YooJunsang3": "유준상",
+        "JungYonghwa3": "정용화",
+        "HanYeSeul3": "한예슬"
     ]
     
     // 브랜드 카테고리
-    static let brands_easy: [String: String] = [
-        "구글": "구글",
-        "나이키": "나이키",
-        "맥도날드": "맥도날드",
-        "스타벅스": "스타벅스",
-        "애플": "애플",
-        "삼성": "삼성",
-        "유튜브": "유튜브",
-        "인스타그램": "인스타그램",
-        "페이스북": "페이스북",
-        "펩시": "펩시"
+    static let brands_easy: [ImageName: UserAnswer] = [
+        "Google1": "구글",
+        "Nike1": "나이키",
+        "McDonalds1": "맥도날드",
+        "Starbucks1": "스타벅스",
+        "Apple1": "애플",
+        "Samsung1": "삼성",
+        "Youtube1": "유튜브",
+        "Intstar1": "인스타그램",
+        "Facebook1": "페이스북",
+        "Pepsi1": "펩시"
     ]
     
-    static let brands_medium: [String: String] = [
-        "노브랜드": "노브랜드",
-        "라코스테": "라코스테",
-        "르꼬끄": "르꼬끄",
-        "엔비디아": "엔비디아",
-        "챔피언": "챔피언",
-        "카파": "카파",
-        "테슬라": "테슬라",
-        "퓨마": "퓨마",
-        "프링글스": "프링글스",
-        "KFC": "KFC"
+    static let brands_medium: [ImageName: UserAnswer] = [
+        "Nobrand2": "노브랜드",
+        "Lacoste2": "라코스테",
+        "Lecoq2": "르꼬끄",
+        "Nvidia2": "엔비디아",
+        "Champion2": "챔피언",
+        "Kappa2": "카파",
+        "Tesla2": "테슬라",
+        "Puma2": "퓨마",
+        "Pringles2": "프링글스",
+        "Kfc2": "KFC"
     ]
     
-    static let brands_hard: [String: String] = [
-        "구찌": "구찌",
-        "루이비통": "루이비통",
-        "버베리": "버베리",
-        "벤츠": "벤츠",
-        "빈폴": "빈폴",
-        "샤넬": "샤넬",
-        "아우디": "아우디",
-        "입생로랑": "입생로랑",
-        "페라리": "페라리",
-        "하메스": "하메스"
+    static let brands_hard: [ImageName: UserAnswer] = [
+        "Gucci3": "구찌",
+        "LouisVuitton3": "루이비통",
+        "Burberry3": "버베리",
+        "Benz3": "벤츠",
+        "Beanpole3": "빈폴",
+        "Chanel3": "샤넬",
+        "Audi3": "아우디",
+        "YvesSaintLaurent3": "입생로랑",
+        "Ferrari3": "페라리",
+        "Hermes3": "하메스"
     ]
     
+    
+    typealias ImageName = String
+    typealias UserAnswer = String
 
     enum Category: CaseIterable, Codable {
         case animals, celebrities, brands
